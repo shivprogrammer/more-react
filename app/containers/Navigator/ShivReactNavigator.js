1298bbf3490bd082b7a28ba812f0388ec16d495e
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-10 14:33:12 -0800

    added renderScene  and configureScene to Navigator

--- app/containers/Navigator/ShivReactNavigator.js
+++ app/containers/Navigator/ShivReactNavigator.js
@@ -1,10 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 import { View, Text, Navigator } from 'react-native';
+import { SplashContainer } from '~/containers';
 
 export default class ShivReactNavigator extends Component {
+  renderScene = (route, navigator) => {
+    <SplashContainer navigator={navigator} />
+  }
+  configureScene = (route) => {
+
+  }
+
   render() {
     return (
-      <Navigator />
+      <Navigator
+        renderScene={this.renderScene}
+        configureScene={this.configureScene} />
     )
   }
 }

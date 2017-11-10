17d9b9e3bec7b79cd0a8b3a27e167c3b629636ea
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-10 14:20:22 -0800

    added Navigator folder in app with index.js, and ShivReactNavigator.js [Both devices working]

---
+++ app/containers/Navigator/ShivReactNavigator.js
@@ -0,0 +1,10 @@
+import React, { PropTypes, Component } from 'react';
+import { View, Text, Navigator } from 'react-native';
+
+export default class ShivReactNavigator extends Component {
+  render() {
+    return (
+      <Navigator />
+    )
+  }
+}
---
+++ app/containers/index.js
@@ -0,0 +1,1 @@
+export ShivReactNavigator from './Navigator/ShivReactNavigator';

ef9ee891a8dbd1fed078c46549d1abf1bb3dbfa5
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-10 14:46:29 -0800

    added components with index.js, and Splash component with Splash.js

---
+++ app/components/Splash/Splash.js
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react';
+import { View, Stylesheet, Text } from 'react-native';
+
+Splash.propTypes = {
+
+}
+
+export default function Splash (props) {
+  return (
+    <View>
+      <Text>
+        Splash
+      </Text>
+    </View>
+  )
+}
+
+const styles = Stylesheet.create({
+  
+})
